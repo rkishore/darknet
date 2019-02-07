4c0fe61057b023a038bc3a30ede20b14a23e26d2
Kishore Iyer <keyshawn@gmail.com>
2019-02-07 10:28:20 -0600

    Bugfix to make sure code compiles with DEBUG=1 without errors about cvRound

--- src/detector.c
+++ src/detector.c
@@ -33,6 +33,10 @@ void draw_train_loss(IplImage* img, int img_size, float avg_loss, float max_img_
 
 #include "http_stream.h"
 
+// To make DEBUG=1 compile without errors about cvRound
+#include "math.h"
+int cvRound(double value) {return(ceil(value));}
+
 int check_mistakes;
 
 static int coco_ids[] = {1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,31,32,33,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,67,70,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,88,89,90};
